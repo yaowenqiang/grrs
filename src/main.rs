21f53772691df554299094f2bda9de5ca66f330f
Yaowenqiang <yaowenqiang111@163.com>
2020-04-03 23:56:03 +0800

    add pattern match

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,14 @@ fn main() {
     */
 
     let args = Cli::from_args();
+    println!("{:?}", args);
+    let contents = std::fs::read_to_string(&args.path).expect("could not load file ");
+
+    for line in contents.lines() {
+       if(line.contains(&args.pattern))  {
+           println!("{:?}", line);
+       }
+    }
 
     println!("{:?}", args);
 
